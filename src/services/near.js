2b6a056c403a580be99ee6d34224fbe9731f201c
OlexandrSai <38455192+OlexandrSai@users.noreply.github.com>
2021-09-10 17:19:23 +0300

    init with newArchitecture

---
+++ src/services/near.js
@@ -0,0 +1,81 @@
+import { keyStores, Near, WalletConnection, utils } from "near-api-js";
+import BN from "bn.js";
+
+export const CONTRACT_ID = "dev-1631282203902-90798358713232";
+const gas = new BN("70000000000000");
+
+export const near = new Near({
+    networkId: "testnet",
+    keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+    nodeUrl: "https://rpc.testnet.near.org",
+    walletUrl: "https://wallet.testnet.near.org",
+  });
+
+  export const wallet = new WalletConnection(near, "lottery");
+
+  //function to get explanation about lottery 
+  export const explainLottery = () => {
+      return wallet.account().viewFunction(CONTRACT_ID, "explain_lottery")
+  }
+
+  //function to get fee of lottery 
+  export const getFee = () => {
+    return wallet.account().viewFunction(CONTRACT_ID, "get_fee")
+    }
+
+    //function to get pot
+  export const getPot = () => {
+    return wallet.account().viewFunction(CONTRACT_ID, "get_pot")
+    }
+
+    //get last played
+  export const getLastPlayed = () => {
+        return wallet.account().viewFunction(CONTRACT_ID, "get_last_played")
+        }
+
+        //is lottery played or no
+        export const getHasPlayed = () => {
+            return wallet.account().viewFunction(CONTRACT_ID, "get_has_played")
+            }
+
+//get winner of lottery, if exists
+export const getWinner = () => {
+    return wallet.account().viewFunction(CONTRACT_ID, "get_winner")
+    }
+    
+//get owner of lottery
+export const getOwner = () => {
+    return wallet.account().viewFunction(CONTRACT_ID, "get_owner")
+    }
+
+//get fee strategy  of lottery
+export const getFeeStrategy = () => {
+    return wallet.account().viewFunction(CONTRACT_ID, "get_fee_strategy")
+    }
+
+//function to play lottery
+export const play = () => {
+      let response
+      let feeNumber = getFee().match(/(\d+)/)[0] //* 1000000000000000000000000
+      feeNumber = feeNumber*1000000000000000
+      console.log(feeNumber)
+      if (this.getHasPlayed()) {
+          //+fee
+        response = wallet.account().functionCall({
+        contractId: CONTRACT_ID,
+        methodName: "say",
+        gas,
+
+        }) 
+      }else{
+        response = wallet.account().functionCall({
+            contractId: CONTRACT_ID,
+            methodName: "say",
+            gas,
+    
+            }) 
+        }
+      console.log(response)
+}
+
+
---
+++ src/views/Home.vue
@@ -0,0 +1,80 @@
+<template>
+  <!--Decoration -->
+  <DecorationDots/>
+  <DecorationLines/>
+  <DecorationCircleLg/>
+  <DecorationCircleMd/>
+  <DecorationCircleSm/>
+
+<!-- Header-->
+<header>
+  <Navigation :isSignedIn='isSignedIn' :accountName='accountName' :balance='accountBalance' v-on:signIn='signIn' v-on:signOut='signOut'/>
+  <PageTitle :chance='chance'
+             :owner='owner'
+             :winner='winner' 
+             :fee='fee' 
+             :pot='pot'
+             :fee_strategy='fee_strategy'
+             :has_played='has_played'
+             v-on:play="play"/>
+  <Options :isOwner='isOwner'/>
+</header>
+
+<Footer/>
+
+ <loading v-model:active="loading"
+                 :can-cancel="true"
+                 :is-full-page="fullPage"/>
+</template>
+
+<script>
+import DecorationDots from  '@/components/DecorationDots.vue'
+import DecorationLines from '@/components/DecorationLines.vue'
+import DecorationCircleLg from '@/components/DecorationCircleLg.vue'
+import DecorationCircleMd from '@/components/DecorationCircleMd.vue'
+import DecorationCircleSm from '@/components/DecorationCircleSm.vue'
+import Navigation from '@/components/Navigation.vue'
+import PageTitle from '@/components/PageTitle.vue'
+import Options from '@/components/Options.vue'
+import Footer from '@/components/Footer.vue'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
+
+//const FeeStrategies = ['Free','Constant','Linear','Exponential']
+
+
+export default {
+  data () {
+    return {
+      loading:false,
+      fullpage:false,
+      isSignedIn: false,
+      accountName: '',
+      accountBalance: '',
+      chance: '20%',
+      fee:'0',
+      pot: '',
+      last_played : '',
+      has_played : false,
+      winner : '',
+      owner: 'NEAR',
+      isOwner: false,
+      fee_strategy: 'Linear'
+    }
+  },
+  name: 'App',
+  components: {
+    DecorationDots,
+    DecorationLines,
+    DecorationCircleLg,
+    DecorationCircleMd,
+    DecorationCircleSm,
+    Navigation,
+    PageTitle,
+    Options,
+    Footer,
+    Loading
+  },
+}
+</script>
