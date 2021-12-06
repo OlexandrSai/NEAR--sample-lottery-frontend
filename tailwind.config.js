c1b1ebc6d0fd18df455f5e1486b815df422f66e4
NazarH2ONyzhnyk <nazarh2onyzhnyk@gmail.com>
2021-12-06 16:45:41 +0200

    popup for branch newArchitecture

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '500': '500px',
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      scale: ['active'],
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
