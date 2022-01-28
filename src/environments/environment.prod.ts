4427fc8829bf20f44d1f7645283bdcf2be72f616
mihail.shevchuk <mihail.s@hubx.capital>
2022-01-28 13:50:23 +0200

    NG | production env fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  NG_APP_CONTRACT_ID: "dev-1642429674498-96921317088281",
+  NG_APP_gas: "70000000000000",
+  NG_APP_networkId: "testnet",
+  NG_APP_nodeUrl: "https://rpc.testnet.near.org",
+  NG_APP_walletUrl: "https://wallet.testnet.near.org"
 };
